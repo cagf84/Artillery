3467d3fe390d4fcd7adb74587637ff155c5664b8
cagf84 <cagf.02@gmail.com>
2024-10-29 23:40:54 -0300

    Tarea-Modulo-17
    Tarea-Modulo-17

---
+++ Assets/_Script/AdministradorJuego.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdministradorJuego : MonoBehaviour
+{
+    public static AdministradorJuego SingletonAdministradorJuego;
+    public static int VelocidadBala = 30;
+    public static int DisparosPorJuego = 7;
+    public static float VelocidadRotacion = 1;
+
+    public void Awake()
+    {
+        if (SingletonAdministradorJuego == null)
+        {
+            SingletonAdministradorJuego = this;
+        }
+        else
+        {
+            Debug.LogError("Ya existe una instancia de esta clase");
+        }
+    }
+}
---
+++ Assets/_Script/Canon.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    public static bool Bloqueado;
+
+    public AudioClip clipDisparo;
+    private GameObject SonidoDisparo;
+    private AudioSource SourceDisparo;
+
+    [SerializeField] private GameObject BalaPrefab;
+    public GameObject ParticulasDisparo;
+    private GameObject puntaCanon;
+    private float rotation;
+
+    private void Start()
+    {
+        puntaCanon = transform.Find("PuntaCanon").gameObject;
+        SonidoDisparo = GameObject.Find("SonidoDisparo");
+        SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        rotation += Input.GetAxis("Horizontal") * AdministradorJuego.VelocidadRotacion;
+        if (rotation <= 90 && rotation >= 0)
+        {
+            transform.eulerAngles = new Vector3(rotation, 90, 0.0f);
+        }
+
+        if (rotation > 90) rotation = 90;
+        if (rotation < 0) rotation = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space) && !Bloqueado)
+        {
+            GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+
+            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;
+            Vector3 direccionDisparo = transform.rotation.eulerAngles;
+            direccionDisparo.y = 90 - direccionDisparo.x;
+            Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particulas = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
+            tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
+            //SourceDisparo.PlayOneShot(clipDisparo);
+            SourceDisparo.Play();
+            Bloqueado = true;
+
+        }
+    }
+}
